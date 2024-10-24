aba16a9dc8db1e72c089712cca39030829f8a9fc
awannoman <160035058+awannoman@users.noreply.github.com>
2024-10-24 12:20:05 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+// summary
+
+// primitive
+// let Name = "noman"; string
+// let age = 12; number
+// let isLogged = true; Boolean
+// let hello; undefined
+// let a = null null
+// let num = 67734632784n bigInt
+// let id = Symbol("123")
+// let anotherId = Symbol("123")
+// console.log(id = anotherId)
+
+// non-primitive
+//  Object , Array ,Function
+// let friends = ['umer','safee','adnan'] Array                  
+// let myObj = {
+//     firstName: "Noman",
+//     lastName: "Awan"
+// }
+// let myFunction = function(){
+//     console.log("hello world");
+// }
+
+
+
+
